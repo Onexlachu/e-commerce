59bfa3d74e6e0c27414303e382c5b32d230845b7
ONEX <113091481+Onexlachu@users.noreply.github.com>
2023-12-11 15:26:30 +0100

    role

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,7 +38,7 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        dd($request->all());
+        // dd($request->all());
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
